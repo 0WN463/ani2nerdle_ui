88ba114866049db67161796361d1092bb3d9fecb
0WN463 <>
2024-11-08 02:41:12 +0800

    Scaffold for starting game

--- src/lobby/Lobby.tsx
+++ src/lobby/Lobby.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 import { nanoid } from "nanoid";
@@ -9,8 +9,67 @@ const URL =
 console.log(URL);
 export const socket = io(URL);
 
-const Lobby = () => {
+type AnimeDetails = {
+  id: number;
+  title?: string;
+  englishTitle?: string;
+  imageUrl?: string;
+};
+
+const AnimeCard = ({ details }: { details: AnimeDetails }) => {
+  return (
+    <>
+      <header>{details?.title}</header>
+      <img src={details.imageUrl} alt={details?.title} />
+    </>
+  );
+};
+
+const Lobby = ({ onStartGame }: { onStartGame: () => void }) => {
+  /* TODO: Add:
+   * handicap
+   * language selector
+   * player indicators
+   * */
+
+  return (
+    <>
+      <div> lobby </div>
+      <button onClick={onStartGame}> Start </button>
+    </>
+  );
+};
+
+const Game = ({ id }: { id: number }) => {
+  const [details, setDetails] = useState<AnimeDetails>();
+
+  useEffect(() => {
+    const getDetails = async () => {
+      const rawResponse = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
+
+      const res = await rawResponse.json();
+
+      const result = {
+        id,
+        title: res.data?.title,
+        title_english: res.data?.title_english,
+        imageUrl: res.data?.images?.webp?.image_url,
+      };
+
+      setDetails(result);
+    };
+
+    getDetails();
+  }, [id]);
+
+  return details ? <AnimeCard details={details} /> : null;
+};
+
+type Stage = { type: "lobby" } | { type: "game"; animeId: number };
+
+const Page = () => {
   const { id } = useParams();
+  const [stage, setStage] = useState<Stage>({ type: "lobby" });
 
   useEffect(() => {
     socket.emit("join_game", { game_id: id, player_id: nanoid() });
@@ -18,20 +77,36 @@ const Lobby = () => {
 
   useEffect(() => {
     const onPlayerJoin = (data: any) => console.log("player joined", data);
+    const onGameStart = (animeId: number) => {
+      console.log("game started", animeId);
+      setStage({ type: "game", animeId });
+    };
+
     socket.on("player joined", onPlayerJoin);
+    socket.on("start game", onGameStart);
 
     return () => {
       socket.off("player joined", onPlayerJoin);
+      socket.off("start game", onGameStart);
     };
   });
-  const game = (
-    <>
-      <div> lobby {id} </div>{" "}
-      <button onClick={() => socket.emit("message", "yo")}> Send </button>
-    </>
-  );
+
+  const startGame = () => {
+    console.log("start game");
+    socket.emit("start game");
+  };
+
+  const game =
+    stage.type === "lobby" ? (
+      <Lobby onStartGame={startGame} />
+    ) : (
+      <>
+        <div> game {id} </div>
+        <Game id={stage.animeId} />
+      </>
+    );
 
   return game;
 };
 
-export default Lobby;
+export default Page;
