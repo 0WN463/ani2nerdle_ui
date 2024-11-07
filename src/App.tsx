a7d2a93de12df4835306ff82304a0b2d0d5a3d0d
0WN463 <>
2024-11-07 11:34:40 +0800

    Initial test for sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+console.log(URL)
+export const socket = io(URL);
 
 function App() {
+  const foo = () => {
+	  console.log("sending to socket")
+	socket.emit("message", "yo")
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+	<button onClick={foo}> Send </button>
         <a
           className="App-link"
           href="https://reactjs.org"
