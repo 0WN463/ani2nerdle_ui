4305af152d0cf0b0bd8d6734d35240afd17e01a1
0WN463 <>
2024-11-07 15:06:57 +0800

    Install and run Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,41 @@
-import './App.css';
-import { io } from 'socket.io-client';
-import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import "./App.css";
+import { io } from "socket.io-client";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
-console.log(URL)
+const URL =
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+console.log(URL);
 export const socket = io(URL);
 
 function App() {
-    const navigate  = useNavigate();
+  const navigate = useNavigate();
 
   const createRoom = async () => {
-	    const rawResponse = await fetch(URL + '/game', {
-	    method: 'POST',
-	  });
+    const rawResponse = await fetch(URL + "/game", {
+      method: "POST",
+    });
 
-	  const content = await rawResponse.text();
-		    navigate("/game/" + content);
-	  }
+    const content = await rawResponse.text();
+    navigate("/game/" + content);
+  };
 
-
-    
-  const home = <div className="App">
+  const home = (
+    <div className="App">
       <header className="Ani2Nerdle">
-	<button onClick={createRoom}> Create Room </button>
-	<Link to="game"> Game </Link>
+        <button onClick={createRoom}> Create Room </button>
+        <Link to="game"> Game </Link>
       </header>
     </div>
+  );
 
-
-    const game = <div> game </div>
-
+  const game = <div> game </div>;
 
   return (
-         <Routes>
-            <Route path="/" element={home} />
-            <Route path="/game/:id" element={game} />
-         </Routes>
+    <Routes>
+      <Route path="/" element={home} />
+      <Route path="/game/:id" element={game} />
+    </Routes>
   );
 }
 
