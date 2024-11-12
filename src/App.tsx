cc9b5931296722e94d283bb1313c6c47c35d7906
0WN463 <>
2024-11-13 01:03:15 +0800

    Optimize react query settings for non-changing Jikan API data

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      retryDelay: 1500,
+      cacheTime: Infinity,
+      staleTime: Infinity,
     },
   },
 });
