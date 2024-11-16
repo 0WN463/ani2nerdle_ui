34cb2ae087d8c3bb684c1c9fc15c0f878399ff42
0WN463 <>
2024-11-16 12:01:40 +0800

    Migrate Lobby to tailwind css

--- src/lobby/Lobby.tsx
+++ src/lobby/Lobby.tsx
@@ -28,16 +28,12 @@ const Lobby = ({
   };
 
   return (
-    <div className="App" style={{ position: "relative", height: "100vh" }}>
-      <div
-        style={{
-          top: "80%",
-          position: "absolute",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <button style={{ fontSize: "xx-large" }} onClick={startGame}>
+    <div className="relative h-screen">
+      <div className="absolute top-3/4 left-1/2 flex flex-col gap-10 -translate-y-1/2 -translate-x-1/2">
+        <button
+          className="text-3xl bg-sky-300 rounded mx-3 p-3"
+          onClick={startGame}
+        >
           Start
         </button>
       </div>
