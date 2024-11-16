d11b89c79ce9e886c51ce39329996f24149cc664
0WN463 <>
2024-11-16 11:00:42 +0800

    Migrate solo lobby to tailwind css

--- src/lobby/LobbySolo.tsx
+++ src/lobby/LobbySolo.tsx
@@ -11,30 +11,33 @@ const LinkLimitOption = ({
   onLimitChange: (_: LinkLimit) => void;
 }) => (
   <label>
-    Link Limit
-    <label>
-      Unlimited
+    Link Limit:
+    <div className="flex gap-2">
+      <label>
+        Unlimited
+        <input
+          type="checkbox"
+          checked={limit.type === "unlimited"}
+          onChange={() =>
+            onLimitChange(
+              limit.type === "limited"
+                ? { type: "unlimited" }
+                : { type: "limited", amt: 3 },
+            )
+          }
+        />
+      </label>
       <input
-        type="checkbox"
-        checked={limit.type === "unlimited"}
-        onChange={() =>
-          onLimitChange(
-            limit.type === "limited"
-              ? { type: "unlimited" }
-              : { type: "limited", amt: 3 },
-          )
+        type="number"
+        className="border border-gray-500 rounded"
+        value={limit.type === "limited" ? limit.amt : ""}
+        disabled={limit.type !== "limited"}
+        min={1}
+        onChange={(v) =>
+          onLimitChange({ type: "limited", amt: parseInt(v.target.value) })
         }
       />
-    </label>
-    <input
-      type="number"
-      value={limit.type === "limited" ? limit.amt : ""}
-      disabled={limit.type !== "limited"}
-      min={1}
-      onChange={(v) =>
-        onLimitChange({ type: "limited", amt: parseInt(v.target.value) })
-      }
-    />
+    </div>
   </label>
 );
 
@@ -49,17 +52,10 @@ const Lobby = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="App" style={{ position: "relative", height: "100vh" }}>
-      <div
-        style={{
-          top: "80%",
-          position: "absolute",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
+    <div className="relative h-screen">
+      <div className="absolute top-3/4 left-1/2 flex flex-col gap-10 -translate-y-1/2 -translate-x-1/2">
         <button
-          style={{ fontSize: "xx-large" }}
+          className="text-3xl bg-sky-300 rounded mx-3 p-3"
           onClick={() =>
             navigate({
               pathname: "/solo/game",
