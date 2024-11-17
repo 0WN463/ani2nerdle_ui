efc5b0605bee24f53d956499f40be8b0d6f99ece
0WN463 <>
2024-11-18 02:09:36 +0800

    Install vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
-})
+  },
+});
