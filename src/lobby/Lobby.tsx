e47c00f6b48bf1397b20e74cf385b75cee600ca1
0WN463 <>
2024-11-30 17:30:49 +0800

    Disable start game if there is no opponent

--- src/lobby/Lobby.tsx
+++ src/lobby/Lobby.tsx
@@ -81,8 +81,9 @@ const Lobby = ({
       <div className="absolute top-3/4 left-1/2 flex flex-col gap-10 -translate-y-1/2 -translate-x-1/2">
         {status === "host" ? (
           <button
-            className="text-3xl bg-sky-300 rounded mx-3 p-3"
+            className="text-3xl bg-sky-300 rounded mx-3 p-3 disabled:bg-sky-100"
             onClick={startGame}
+            disabled={!opponent}
           >
             Start
           </button>
